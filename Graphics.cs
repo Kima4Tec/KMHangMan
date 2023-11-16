57d60ff300f0e10623f2755c905242cff7f501e9
Kim Lasse Massesson <kima4@elev.tec.dk>
2023-11-16 13:56:15 +0100

    Add project files.

---
+++ Graphics.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMHangMan
+{
+    internal class Graphics
+    {
+        //static void logo(int x, int y, ConsoleColor color = ConsoleColor.Yellow)
+        //{
+        //    Console.ForegroundColor = color;
+        //    Pos(x, y + 0, "       _    _                   __  __                 ");
+        //    Pos(x, y + 1, "      | |  | |                 |  \/  |                ");
+        //    Pos(x, y + 2, "      | |__| | __ _ _ __   __ _| \  / | __ _ _ __      ");
+        //    Pos(x, y + 3, "      |  __  |/ _` | '_ \ / _` | |\/| |/ _` | '_ \     ");
+        //    Pos(x, y + 4, "      | |  | | (_| | | | | (_| | |  | | (_| | | | |    ");       
+        //    Pos(x, y + 5, "      |_|  |_|\__,_|_| |_|\__, |_|  |_|\__,_|_| |_|    ");
+        //    Pos(x, y + 6, "                           __/ |                       ");
+        //    Pos(x, y + 7, "                           |___/                       ");
+
+        //}
+
+
+        static void Pos(int x, int y, string tekst) //positioning text
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(tekst);
+        }
+        static void Frames() //Frames araound logo and textfield
+        {
+
+
+            //1. horizontal line
+            for (int i = 0; i < 67; i++)
+            {
+                Pos(21 + i, 1, "─");
+            }
+            //2. horizontal line
+            for (int i = 0; i < 67; i++)
+            {
+                Pos(21 + i, 10, "─");
+            }
+            //3. horizontal line
+            for (int i = 0; i < 67; i++)
+            {
+                Pos(21 + i, 24, "─");
+            }
+            //left vertical line
+            for (int i = 0; i < 22; i++)
+            {
+                Pos(20, 2 + i, "│");
+            }
+            //right vertical line
+            for (int i = 0; i < 22; i++)
+            {
+                Pos(88, 2 + i, "│");
+            }
+            //left top corner
+            Pos(20, 1, "┌");
+
+            //right top corner
+            Pos(88, 1, "┐");
+
+            //left m corner
+            Pos(20, 10, "├");
+
+            //right m corner
+            Pos(88, 10, "┤");
+
+            //left m corner
+            Pos(20, 24, "└");
+
+            //right m corner
+            Pos(88, 24, "┘");
+
+        }
+        static void EmptyBox(int x, int y) //clearing text
+        {
+            Pos(x, y + 0, "                                                                   ");
+            Pos(x, y + 1, "                                                                   ");
+            Pos(x, y + 2, "                                                                   ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace KMHangMan
+{
+    internal class Program
+    {
+        public static string[] secretWords = { "mclaren", "porsche", "lamborghini", "tuatara", "mercedes", "ferrari", "bentley", "zenvo", "koenigsegg", "bugatti" };
+        public static bool IsWordGuessed = false;
+        public static int guessWrongNumber = 0;
+
+
+        static void Main(string[] args)
+        {
+            StartHangman();
+        }
+        public static void StartHangman()
+        {
+            //random instance
+            Random rnd = new Random();
+            //get a random number which is in the scope of the array secretWords
+            int number = rnd.Next(1, secretWords.Length);
+            //create a varible containing the secret word
+            string secretWordPicked = secretWords[number];
+            //write the secret word
+            Console.WriteLine(secretWordPicked);
+            //find the length of the secret word picked
+            int GetLineLength = secretWordPicked.Length;
+            //create a variable with - in the length of the secret word
+            string userWord = new string('-', GetLineLength);
+            //write the secret word
+            Console.WriteLine(userWord);
+            ConvertWordToArray(secretWordPicked, userWord);
+        }
+
+        public static void ConvertWordToArray(string secretWord, string userWord)
+        {
+            //break up the word in char and put in a new array
+            //check if the array contains the letter
+            char[] wordInChar = secretWord.ToCharArray();
+            char[] userWordInChar = userWord.ToCharArray();
+            getUserInput(wordInChar, userWordInChar);
+        }
+
+        //char[] userWordChar = secretWordPicked.ToCharArray();
+        public static void getUserInput(char[] wordInChar, char[] userWordInChar)
+        {
+            Console.WriteLine();
+            Console.Write("Write a letter: ");
+            char userInputKey = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            checkWord(wordInChar, userWordInChar, userInputKey);
+        }
+
+        private static void checkWord(char[] wordInChar, char[] userWordInChar, char userInputKey)
+        {
+            bool letterFound = false;
+
+            while (!IsWordGuessed)
+            {
+                for (int i = 0; i < wordInChar.Length; i++)
+                {
+                    if (wordInChar[i] == userInputKey)
+                    {
+                        letterFound = true;
+                        userWordInChar[i] = userInputKey;
+                    }
+                }
+
+                if (!letterFound)
+                {
+                    Console.WriteLine($"{userInputKey} is not in the word.");
+                    guessWrongNumber++;
+                }
+                foreach (char s in userWordInChar)
+                {
+                    Console.Write(s);
+                }
+                if (guessWrongNumber > 4)
+                {
+                    IsWordGuessed = true;
+                }
+                IsWordFound(wordInChar, userWordInChar);
+            }
+        }
+        public static void IsWordFound(char[] wordInChar, char[] userWordInChar)
+        {
+            if(guessWrongNumber != 5)
+            {
+                if (wordInChar.SequenceEqual(userWordInChar))
+                {
+                    IsWordGuessed = true;
+                    UserWon();
+                }
+                else
+                {
+                    getUserInput(wordInChar, userWordInChar);
+                }
+            }
+            else
+            {
+                Fail(wordInChar);
+            }
+
+
+        }
+        public static void UserWon()
+        {
+            Console.WriteLine("\nYou won");
+        }
+
+        public static void Fail(char[] wordInChar)
+        {
+            string secretWordRevealed = new string(wordInChar);
+            Console.WriteLine($"You failed guessing the word. The word was: {secretWordRevealed}");
+
+        }
+    }
+}
