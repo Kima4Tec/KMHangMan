29300f791f979db4f2a1295566874bc6673d3061
Kim Lasse Massesson <kima4@elev.tec.dk>
2023-11-19 01:26:50 +0100

    rewrote code and flowchart

--- FixedData.cs
+++ FixedData.cs
@@ -6,8 +6,73 @@ using System.Threading.Tasks;
 
 namespace KMHangMan
 {
-    internal class Graphics
+    internal class FixedData
     {
+        public static string[] secretWordsArray = { "mclaren", "porsche", "lamborghini", "tuatara", "mercedes", "ferrari", "bentley", "zenvo", "koenigsegg", "bugatti", "jaguar" };
+        public static void IntroText(int y) 
+        {
+            string gameInfo1 = @"HangMan is a game in which one player tries to guess ";
+            string gameInfo2 = @"letters of a word. In this game you have to guess";
+            string gameInfo3 = @"the word of a special car. If you fail to find the";
+            string gameInfo4 = @"right letter after six times, you hang.";
+            Pos(((Console.WindowWidth) - gameInfo1.Length) / 2, y + 0, gameInfo1, ConsoleColor.DarkRed);
+            Pos(((Console.WindowWidth) - gameInfo1.Length) / 2, y + 1, gameInfo2, ConsoleColor.Red);
+            Pos(((Console.WindowWidth) - gameInfo1.Length) / 2, y + 2, gameInfo3, ConsoleColor.DarkYellow);
+            Pos(((Console.WindowWidth) - gameInfo1.Length) / 2, y + 3, gameInfo4, ConsoleColor.Yellow);
+        }
+
+        public static void PrintInfoOnObscuredText(int y)
+        {
+            string infoText = "This is the word obscured:";
+            Pos(((Console.WindowWidth) - infoText.Length) / 2, y + 0, infoText, ConsoleColor.White);
+        }
+
+        public static void PrintKeyText(int y)
+        {
+            string writeText = "Write a letter:";
+            Pos(((Console.WindowWidth) - writeText.Length) / 2, y + 0, writeText, ConsoleColor.White);
+        }
+
+        public static void PrintObscuredWord(int y)
+        {
+            Pos(((Console.WindowWidth) - Program.obscuredWord.Length) / 2, y + 0, Program.obscuredWord, ConsoleColor.White);
+        }
+
+        public static void PrintUsedLetter(int y)
+        {
+            Pos(27, y, Program.usedLetters.PadRight(15), ConsoleColor.Cyan);
+        }
+
+        public static void PrintKeyNotValid(int y)
+        {
+            string KeyNotValid = "You have to press a letter.";
+            Pos(((Console.WindowWidth) - KeyNotValid.Length) / 2, y, KeyNotValid, ConsoleColor.Red);
+        }
+
+        public static void PrintPlayAgain(int y)
+        {
+            string playAgainText = "Play again? (Y/N)";
+            Pos(((Console.WindowWidth) - playAgainText.Length) / 2, y, playAgainText, ConsoleColor.Green);
+        }
+
+        public static void PrintYouLost(int y)
+        {
+            string youLostText = "You failed guessing the word.";
+            Pos(((Console.WindowWidth) - youLostText.Length) / 2, y, youLostText.PadRight(30), ConsoleColor.Red);
+        }
+        public static void PrintYouWin(int y)
+        {
+            string youLostText = "You guessed the word. You win!";
+            Pos(((Console.WindowWidth) - youLostText.Length) / 2, y, youLostText.PadRight(30), ConsoleColor.Green);
+        }
+
+
+        public static void EraseoldText()
+        {
+            Graphics.Pos(((Console.WindowWidth) - 30) / 2, 22, "".PadRight(30));
+            Graphics.Pos(((Console.WindowWidth) - 30) / 2, 23, "".PadRight(30));
+        }
+
         public static void Logo(int x, int y)
         {
             LogoPos(x, y + 0, @"       _    _                   __  __                   ");
@@ -20,7 +85,7 @@ namespace KMHangMan
             LogoPos(x, y + 7, @"                           |___/                         ");
 
         }
-        public static void DrawGallow(int x, int y) 
+        public static void DrawGallow(int x, int y)
         {
             Pos(x, y + 0, @"   _______", ConsoleColor.DarkYellow);
             Pos(x, y + 1, @"  |/    | ", ConsoleColor.DarkYellow);
--- Program.cs
+++ Program.cs
@@ -9,273 +9,224 @@ namespace KMHangMan
 {
     internal class Program
     {
-        public static string[] secretWords = { "mclaren", "porsche", "lamborghini", "tuatara", "mercedes", "ferrari", "bentley", "zenvo", "koenigsegg", "bugatti","jaguar" };
-        public static bool IsWordGuessed = false;
-        public static int guessWrongNumber = 0;
-        public static string points = "Life: OOOOO";
+        public static int wrongGuessNumber;
+        public static string usedLetters = "";
+        public static string secretWord = "";
+        public static string obscuredWord = "";
+        
 
         static void Main(string[] args)
         {
-            PreGame();
+            Setup();
         }
-        public static void PreGame()
+
+        static void Setup()
         {
-            //setting up text and frames
-            SettingUp();
+            Console.Clear();
+            FixedData.Logo(((Console.WindowWidth) - 57) / 2, 2);
+            FixedData.Frames();
+            FixedData.IntroText(11);
+            FixedData.PrintInfoOnObscuredText(16);
+            FixedData.DrawGallow(29, 16);
+            FixedData.DrawTree(79, 16);
+            FixedData.PrintKeyText(20);
 
-            //starting the game
-            StartHangman();
+
+            ProcessWord();
         }
 
-        private static void SettingUp()
-        {
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Graphics.Logo(((Console.WindowWidth) - 57) / 2, 2);
-            Graphics.Frames();
-            string description1 = "HangMan is a game in which one player tries to guess ";
-            string description2 = "the letters of a word. In this game you have to guess";
-            string description3 = "the word of a special car. If you fail to find the";
-            string description4 = "right letter after five times, you hang.";
-            Graphics.Pos(((Console.WindowWidth) - description1.Length) / 2, 11, description1, ConsoleColor.DarkRed);
-            Graphics.Pos(((Console.WindowWidth) - description1.Length) / 2, 12, description2, ConsoleColor.Red);
-            Graphics.Pos(((Console.WindowWidth) - description1.Length) / 2, 13, description3, ConsoleColor.DarkYellow);
-            Graphics.Pos(((Console.WindowWidth) - description1.Length) / 2, 14, description4, ConsoleColor.Yellow);
-            Graphics.Pos(27, 23, points, ConsoleColor.Green);
-        }
-
-
-
-        public static void StartHangman()
+        static void ProcessWord()
         {
             //random instance
-            Random rnd = new Random();
+            Random rnd = new();
 
-            //get a random number which is in the scope of the array secretWords
-            int number = rnd.Next(1, secretWords.Length);
+            //get a random number which is in the scope of the array pickWord
+            int number = rnd.Next(1, FixedData.secretWordsArray.Length);
 
             //create a varible containing the secret word
-            string secretWordPicked = secretWords[number];
-
-            //write the secret word
-            Console.WriteLine();
+            secretWord = FixedData.secretWordsArray[number];
 
             //find the length of the secret word picked
-            int GetLineLength = secretWordPicked.Length;
+            int GetLineLength = secretWord.Length;
 
-            //create a variable with - in the length of the secret word
-            string userWord = new string('-', GetLineLength);
+            obscuredWord = new string('-', GetLineLength);
+            wrongGuessNumber = 0;
 
-            //write the secret word
-            string infoText = "This is the word obscured: \n\n";
+            //create a variable with -------- in the length of the secret word
+            FixedData.PrintObscuredWord(18);
 
-            //Color change to white
-            Console.ForegroundColor = ConsoleColor.White;
 
-            //Print out userWord - - - - - 
-            Graphics.Pos(((Console.WindowWidth) - userWord.Length) / 2, 18, userWord, ConsoleColor.White);
-
-            //Print out info text
-            Graphics.Pos(((Console.WindowWidth) - infoText.Length) / 2, 16, infoText, ConsoleColor.White);
-
-            ConvertWordToArray(secretWordPicked, userWord);
+            StartGame();
         }
-
-
-
-            public static void ConvertWordToArray(string secretWord, string userWord)
+        static void StartGame()
         {
-            //break up the word in char and put in a new array
-            //check if the array contains the letter
-            char[] wordInChar = secretWord.ToCharArray();
-            char[] userWordInChar = userWord.ToCharArray();
-
-            //Call method to get user input
-            GetUserInput(wordInChar, userWordInChar);
-        }
-
-        /// <summary>
-        /// Info to user. Getting key and making it lower. Checking if user
-        /// press ENTER og SPACEBAR and tells user to press a letter.
-        /// Call method CheckWord with three arguments: wordInChar, userWordInChar, userInputKey
-        /// </summary>
-        /// <param name="wordInChar"></param>
-        /// <param name="userWordInChar"></param>
-        public static void GetUserInput(char[] wordInChar, char[] userWordInChar)
-        {
-            string userMessage = "Write a character: ";
-            Graphics.Pos(((Console.WindowWidth) - userMessage.Length) / 2, 20, userMessage, ConsoleColor.White);
-            char inputKey = Console.ReadKey().KeyChar;
-            char userInputKey = char.ToLower(inputKey);
-            InfoWrongKeyStroke(userInputKey);
-            switch (userInputKey)
+            char[] obscuredWordChar = obscuredWord.ToCharArray();
+            char[] secretWordChar = secretWord.ToCharArray();
+            while (!IsWordGuessed(obscuredWordChar) && IsThereMoreAttempts())
             {
-                case (char)ConsoleKey.Enter:
-                    GetUserInput(wordInChar, userWordInChar);
-                    break;
-                case (char)ConsoleKey.Spacebar:
-                    GetUserInput(wordInChar, userWordInChar);
-                    break;
-            }
-            EraseoldText();
-            CheckWord(wordInChar, userWordInChar, userInputKey);
-        }
+                Console.SetCursorPosition(68, 20);
+                char inputKey = Console.ReadKey(intercept: true).KeyChar;
+                char userInputKey = char.ToLower(inputKey);
+                IsKeyValid(userInputKey);
 
-        /// <summary>
-        /// clearing old info text
-        /// </summary>
-        private static void EraseoldText()
-        {
-            Graphics.Pos(((Console.WindowWidth) - 21) / 2, 22, "                     ");
-            Graphics.Pos(((Console.WindowWidth) - 27) / 2, 23, "                           ");
-        }
-
-        /// <summary>
-        /// Info when ENTER or SPACEBAR has been pressed.
-        /// </summary>
-        /// <param name="userInputKey"></param>
-        static void InfoWrongKeyStroke(char userInputKey)
-        {
-            string wrongKeys = "You have to press a letter.";
-            Graphics.Pos(((Console.WindowWidth) - wrongKeys.Length) / 2, 23, wrongKeys, ConsoleColor.Cyan);
-            return;
-        }
-
-        /// <summary>
-        /// Checking if the keys pressed is in the secret word while bool IsWordGuessed is false.
-        /// Counts also the times user fails to guess a letter. Points are taken away and shown in left lower corner. Pos(27,23).
-        /// Checking if word is found, and checking if mistakes are 5, which means user lost
-        /// </summary>
-        /// <param name="wordInChar"></param>
-        /// <param name="userWordInChar"></param>
-        /// <param name="userInputKey"></param>
-        private static void CheckWord(char[] wordInChar, char[] userWordInChar, char userInputKey)
-        {
-            bool charFound = false;
-
-            while (!IsWordGuessed)
-            {
-                for (int i = 0; i < wordInChar.Length; i++)
+                if (IsKeyInWord(userInputKey, secretWordChar, obscuredWordChar))
                 {
-                    if (wordInChar[i] == userInputKey)
-                    {
-                        charFound = true;
-                        userWordInChar[i] = userInputKey;
-                    }
-                }
-
-                if (!charFound)
-                {
-                    string letterNotThere = $"{userInputKey} is not in the word.";
-                        Graphics.Pos(((Console.WindowWidth) - letterNotThere.Length) / 2, 22, letterNotThere, ConsoleColor.Red);
-
-                    guessWrongNumber++;
-                    Graphics.Pos(27, 23, "                ");
-                    string newPoints = points.Substring(0, points.Length - guessWrongNumber);
-                    Graphics.Pos(27, 23, newPoints, ConsoleColor.Green);
-                }
-
-                RefreshLineWord(userWordInChar);
-                IsWrongGuessTooMany(guessWrongNumber);
-
-                IsWordFound(wordInChar, userWordInChar);
-            }
-        }
-
-        /// <summary>
-        /// Method returning a bool, checking whether fails of guesses succeds 4.
-        /// When 5 fails, game is over.
-        /// </summary>
-        /// <param name="guessWrongNumber"></param>
-        /// <returns></returns>
-        public static bool IsWrongGuessTooMany(int guessWrongNumber)
-        {
-            if (guessWrongNumber > 4)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Refreshing the char array userWordInChar, after user input
-        /// </summary>
-        /// <param name="userWordInChar"></param>
-        static void RefreshLineWord(char[] userWordInChar)
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.SetCursorPosition(((Console.WindowWidth) - userWordInChar.Length) / 2, 18);
-            foreach (char s in userWordInChar)
-            {
-                Console.Write(s);
-            }
-        }
-
-        /// <summary>
-        /// Checking whether the secret word and if the user has found the right word.
-        /// In case the word is guessed, then IsWordGuessed = true, and user has won.
-        /// Otherwise get user to try to press another key.
-        /// </summary>
-        /// <param name="wordInChar"></param>
-        /// <param name="userWordInChar"></param>
-        public static void IsWordFound(char[] wordInChar, char[] userWordInChar)
-        {
-            if(guessWrongNumber != 5)
-            {
-                if (wordInChar.SequenceEqual(userWordInChar))
-                {
-                    IsWordGuessed = true;
-                    UserWon();
+                    KeyUsed(userInputKey);
                 }
                 else
                 {
-                    GetUserInput(wordInChar, userWordInChar);
+                    KeyUsed(userInputKey);
+                    wrongGuessNumber++;
+                    DrawHangMan();
                 }
+                
+            }
+
+            if (IsWordGuessed(obscuredWordChar))
+            {
+                YouWin();
+            }
+            if (!IsThereMoreAttempts())
+            {
+                YouLose();
+            }
+
+        }
+
+        static char IsKeyValid(char userInputKey)
+        {
+            while (true) {
+                if (!char.IsLetter(userInputKey) && usedLetters.Contains(userInputKey))
+                {
+                    string wrongKeys = "That is not a valid letter.";
+                    Graphics.Pos(((Console.WindowWidth) - wrongKeys.Length) / 2, 23, wrongKeys, ConsoleColor.Cyan);
+                    
+                }
+                else 
+                    return userInputKey;
+ 
+            }
+        }
+
+        static bool IsKeyInWord(char userInputKey, char[] secretWordChar, char[] obscuredWordChar)
+        {
+            if (secretWord.Contains(userInputKey))
+            {
+                for (int i = 0; i < secretWord.Length; i++)
+                {
+                    if (secretWordChar[i] == userInputKey)
+                    {
+                        obscuredWordChar[i] = userInputKey;
+                    }
+                }
+                ShowKeyInWord(obscuredWordChar);
+                return true;
             }
             else
             {
-                Fail(wordInChar);
-            }
-
-            ///Winner message and question to play again
-        }
-        public static void UserWon()
-        {
-            string youWon = "You Won and will stay alive!";
-            Graphics.Pos(((Console.WindowWidth) - youWon.Length) / 2, 22, youWon, ConsoleColor.Green);
-            Console.SetCursorPosition(0, 25);
-            Graphics.Pos(73, 23, "Play again? (Y/N): ", ConsoleColor.Green);
-            if (Console.ReadKey().Key == ConsoleKey.Y)
-            {
-                Console.Clear();
-                guessWrongNumber = 0;
-                points = "Life: OOOOO";
-                IsWordGuessed = false;
-                PreGame();
+                string letterNotThere = $"{userInputKey} is not in the word.";
+                Graphics.Pos(((Console.WindowWidth) - letterNotThere.Length) / 2, 22, letterNotThere, ConsoleColor.Red);
+                return false;
             }
         }
 
-        /// <summary>
-        /// Loser message and question to play again
-        /// </summary>
-        /// <param name="wordInChar"></param>
-        public static void Fail(char[] wordInChar)
+        static bool IsThereMoreAttempts()
         {
-            string secretWordRevealed = new string(wordInChar);
-            string youLost = $"You failed guessing the word. The word was: {secretWordRevealed}";
-            Graphics.Pos(((Console.WindowWidth) - youLost.Length) / 2, 22, youLost, ConsoleColor.Red);
-            Console.SetCursorPosition(0, 25);
-            Graphics.Pos(71, 23, "Play again? (Y/N): ", ConsoleColor.Green);
-            //Get input. Check if input is a "Y"
-            if (Console.ReadKey().Key == ConsoleKey.Y)
+            if (wrongGuessNumber < 6)
             {
-                Console.Clear();
-                guessWrongNumber = 0;
-                points = "Life: OOOOO";
-                IsWordGuessed = false;
-                PreGame();
+                return true;
             }
+             return false;
+        }
 
+        static void DrawHangMan()
+        {
+            switch (wrongGuessNumber)
+            {
+                case 1:
+                    Graphics.Pos(36, 18, @"(_)", ConsoleColor.Gray);
+                    break;
+                case 2:
+                    Graphics.Pos(36, 19, @"/", ConsoleColor.DarkGreen);
+                    break;
+                case 3:
+                    Graphics.Pos(37, 19, @"|", ConsoleColor.DarkGreen);
+                    break;
+                case 4:
+                    Graphics.Pos(38, 19, @"\", ConsoleColor.DarkGreen);
+                    break;
+                case 5:
+                    Graphics.Pos(36, 20, @"/", ConsoleColor.Blue);
+                    break;
+                case 6:
+                    Graphics.Pos(38, 20, @"\", ConsoleColor.Blue);
+                    break;
+            }
+        }
+
+        static void KeyUsed(char userInputKey)
+        {
+            usedLetters += userInputKey;
+            FixedData.PrintUsedLetter(23);
+            Console.SetCursorPosition(0, 27);
+        }
+
+        static void ShowKeyInWord(char[] obscuredWordChar)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(((Console.WindowWidth) - obscuredWordChar.Length) / 2, 18);
+            foreach (char s in obscuredWordChar)
+            {
+                Console.Write(s);
+            }
+            IsWordGuessed(obscuredWordChar);
+        }
+
+        static bool IsWordGuessed(char[] obscuredWordChar)
+        {
+            string obscuredWord = new string(obscuredWordChar);
+            return obscuredWord == secretWord;
+        }
+
+        static void YouLose()
+        {
+            FixedData.PrintYouLost(22);
+            IsPlayAgain();
+        }
+
+        static void YouWin()
+        {
+            FixedData.PrintYouWin(22);
+            IsPlayAgain();
+        }
+
+        static void IsPlayAgain()
+        {
+            FixedData.PrintPlayAgain(23);
+            bool validKeyPress = false;
+            while (!validKeyPress)
+            {
+                ConsoleKeyInfo x = Console.ReadKey();
+
+                if ( x.Key == ConsoleKey.Y)
+                {
+                    usedLetters = "";
+                    FixedData.PrintUsedLetter(23);
+                    FixedData.EraseoldText();
+                    ProcessWord();
+                    validKeyPress = true;
+                }
+                if (x.Key == ConsoleKey.N)
+                {
+                    EndGame();
+                    validKeyPress = true;
+                }
+            }
+        }
+
+        static void EndGame()
+        {
+          
         }
     }
 }
