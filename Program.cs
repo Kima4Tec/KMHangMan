2b49986e985a02c05c46f5b8451446bb05de99aa
Kim Lasse Massesson <kima4@elev.tec.dk>
2023-11-23 11:57:39 +0100

    comments

--- Program.cs
+++ Program.cs
@@ -16,13 +16,15 @@ namespace KMHangMan
         public static string usedLetters = "";
         public static string secretWord = "";
         public static string obscuredWord = "";
-        
+
 
         static void Main(string[] args)
         {
             Setup();
         }
-
+        /// <summary>
+        /// Setting up text and graphic
+        /// </summary>
         static void Setup()
         {
             string[] loadedWordsArray = LoadArrayFromJsonFile(jsonFilePath);
@@ -36,7 +38,11 @@ namespace KMHangMan
             FixedData.PrintKeyText(20);
             ProcessWord(loadedWordsArray);
         }
-
+        /// <summary>
+        /// Loading data from json file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
         static string[] LoadArrayFromJsonFile(string filePath)
         {
             if (File.Exists(filePath))
@@ -49,7 +55,10 @@ namespace KMHangMan
                 return Array.Empty<string>();
             }
         }
-
+        /// <summary>
+        /// Picking a random number 
+        /// </summary>
+        /// <param name="loadedWordsArray"></param>
         static void ProcessWord(string[] loadedWordsArray)
         {
             //random instance
@@ -70,9 +79,17 @@ namespace KMHangMan
             //create a variable with -------- in the length of the secret word
             FixedData.PrintObscuredWord(18);
 
-
             StartGame();
         }
+
+        /// <summary>
+        /// This methods runs the overall of the program. 
+        /// Dividing the secretword up into a char array, and create an array for user input.
+        /// Getting keyinput, and checking it for validity, word guessed and attmepts left calling 
+        /// the different methods.
+        /// Calling DrawHangMan() for wrong answers and adding the number in wrongGuessNumber.
+        /// End result is if you win or lose.
+        /// </summary>
         static void StartGame()
         {
             char[] obscuredWordChar = obscuredWord.ToCharArray();
@@ -85,18 +102,18 @@ namespace KMHangMan
                 if (IsKeyValid(userInputKey))
                 {
                     FixedData.EraseoldText();
-                if (IsKeyInWord(userInputKey, secretWordChar, obscuredWordChar))
-                {
-                    KeyUsed(userInputKey);
+                    if (IsKeyInWord(userInputKey, secretWordChar, obscuredWordChar))
+                    {
+                        KeyUsed(userInputKey);
+                    }
+                    else
+                    {
+                        KeyUsed(userInputKey);
+                        wrongGuessNumber++;
+                        DrawHangMan();
+                    }
                 }
-                else
-                {
-                    KeyUsed(userInputKey);
-                    wrongGuessNumber++;
-                    DrawHangMan();
-                }
-                }
-                
+
             }
 
             if (IsWordGuessed(obscuredWordChar))
@@ -109,7 +126,12 @@ namespace KMHangMan
             }
 
         }
-
+        /// <summary>
+        /// Checking whether the key input is valid and if is already is tried,
+        /// by checking the variable usedLetters, which contains the used letters from user.
+        /// </summary>
+        /// <param name="userInputKey"></param>
+        /// <returns></returns>
         static bool IsKeyValid(char userInputKey)
         {
             if (!char.IsLetter(userInputKey) || usedLetters.Contains(userInputKey))
@@ -118,9 +140,16 @@ namespace KMHangMan
                 Graphics.Pos(((Console.WindowWidth) - wrongKeys.Length) / 2, 23, wrongKeys, ConsoleColor.Cyan);
                 return false;
             }
-                return true; 
+            return true;
         }
 
+        /// <summary>
+        /// Checking whether the input key is a letter in the secretword or not
+        /// </summary>
+        /// <param name="userInputKey"></param>
+        /// <param name="secretWordChar"></param>
+        /// <param name="obscuredWordChar"></param>
+        /// <returns></returns>
         static bool IsKeyInWord(char userInputKey, char[] secretWordChar, char[] obscuredWordChar)
         {
             if (secretWord.Contains(userInputKey))
@@ -142,16 +171,22 @@ namespace KMHangMan
                 return false;
             }
         }
-
+        /// <summary>
+        /// keeping tracks of attempts used. The number is used in the switch, drawing the Hangman
+        /// </summary>
+        /// <returns></returns>
         static bool IsThereMoreAttempts()
         {
             if (wrongGuessNumber < 6)
             {
                 return true;
             }
-             return false;
+            return false;
         }
-
+        /// <summary>
+        /// Drawing the Hangman where each case is initialised by the number of wrong guesses
+        /// Using positioning to place it on the screen
+        /// </summary>
         static void DrawHangMan()
         {
             switch (wrongGuessNumber)
@@ -176,7 +211,10 @@ namespace KMHangMan
                     break;
             }
         }
-
+        /// <summary>
+        /// Adding the key input to the variable usedLetters
+        /// </summary>
+        /// <param name="userInputKey"></param>
         static void KeyUsed(char userInputKey)
         {
             usedLetters += userInputKey;
@@ -184,6 +222,10 @@ namespace KMHangMan
             Console.SetCursorPosition(0, 27);
         }
 
+        /// <summary>
+        /// Writing the status quo of the guessed word 
+        /// </summary>
+        /// <param name="obscuredWordChar"></param>
         static void ShowKeyInWord(char[] obscuredWordChar)
         {
             Console.ForegroundColor = ConsoleColor.White;
@@ -195,24 +237,36 @@ namespace KMHangMan
             IsWordGuessed(obscuredWordChar);
         }
 
+        /// <summary>
+        /// Checking if the word is guessed by comparison of the status quo of the guessed word (obscuredword) and
+        /// the secretword
+        /// </summary>
+        /// <param name="obscuredWordChar"></param>
+        /// <returns></returns>
         static bool IsWordGuessed(char[] obscuredWordChar)
         {
             string obscuredWord = new string(obscuredWordChar);
             return obscuredWord == secretWord;
         }
-
+        /// <summary>
+        /// You lose statement and asking if user wants to play again.
+        /// </summary>
         static void YouLose()
         {
             FixedData.PrintYouLost(22);
             IsPlayAgain();
         }
-
+        /// <summary>
+        /// You win statement and asking if user wants to play again.
+        /// </summary>
         static void YouWin()
         {
             FixedData.PrintYouWin(22);
             IsPlayAgain();
         }
-
+        /// <summary>
+        /// Resetting values and starting the program again. 
+        /// </summary>
         static void IsPlayAgain()
         {
             FixedData.PrintPlayAgain(23);
@@ -221,7 +275,7 @@ namespace KMHangMan
             {
                 ConsoleKeyInfo x = Console.ReadKey();
 
-                if ( x.Key == ConsoleKey.Y)
+                if (x.Key == ConsoleKey.Y)
                 {
                     usedLetters = "";
                     FixedData.PrintUsedLetter(23);
@@ -239,7 +293,7 @@ namespace KMHangMan
 
         static void EndGame()
         {
-          
+
         }
     }
 }
